02a6e374ab3f80f144bd4fd5cb42e6b8cb0f8a13
hamuse <hamuseco@gmail.com>
2019-10-22 15:16:13 +0900

    first commit

---
+++ src/main/java/com/wego/web/config/RootConfig.java
@@ -0,0 +1,31 @@
+package com.wego.web.config;
+
+import javax.sql.DataSource;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@MapperScan(basePackages = {"com.wego.web"})
+@ComponentScan(basePackages = {"com.wego.web"})
+public class RootConfig {
+		@Bean
+		public DataSource dataSource() {
+			HikariConfig hikariConfig = new HikariConfig();
+			hikariConfig.setDriverClassName("com.mysql.jdbc.Driver");
+			hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/wego?serverTimezone=UTC");
+			hikariConfig.setUsername("wego");
+			hikariConfig.setPassword("wego");
+			
+			HikariDataSource dataSource =new HikariDataSource(hikariConfig);
+			
+			return dataSource;
+		}
+	
+	
+}
